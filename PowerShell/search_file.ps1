fd164d50baa6a52bac45d490ff014f8046daf0c8
jinboyasushi <57482581+jinboyasushi@users.noreply.github.com>
2024-04-22 11:30:14 +0900

    Create search_file.ps1

---
+++ PowerShell/search_file.ps1
@@ -0,0 +1,35 @@
+function searchFile($filename){
+
+    $targetDir = 'C:\'
+    $targetFile = $filename
+
+
+    #status flg files
+    $workDir = 'C:\Windows\Temp\'
+    $status_flg_result = $workDir + 'cs_result.txt'
+    $status_flg_done = $workDir + 'cs_done.txt'
+    $status_flg_running = $workDir + 'cs_running.txt'
+
+
+    # check already checked
+    if((Test-Path $status_flg_done) -or (Test-Path $status_flg_running)){
+        exit
+    }
+
+
+    # Start
+    echo '' > $status_flg_running
+    $result = Get-ChildItem $targetDir -Recurse -File -Include $targetFile
+
+
+    # Write result
+    if($result){
+        echo $result > $status_flg_result
+    }
+
+    #Finish
+    rm $status_flg_running
+    echo '' > $status_flg_done
+}
+
+searchFile('shiga.png')
